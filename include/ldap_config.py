dce90cf36ed3552c9126b4207aca0f87c60b5cd5
Ken Jenney <mastermindg@gmail.com>
2017-10-11 15:48:48 -0400

    Passing new variables for modularity

--- include/ldap_config.py
+++ include/ldap_config.py
@@ -19,6 +19,12 @@ AUTH_LDAP_GROUP_SEARCH_ROLE = os.getenv('AUTH_LDAP_GROUP_SEARCH_ROLE')
 ACTIVE_DIRECTORY = os.getenv('ACTIVE_DIRECTORY')
 USER_FIRSTNAME_MAP = os.getenv('USER_FIRSTNAME_MAP')
 USER_LASTNAME_MAP = os.getenv('USER_LASTNAME_MAP')
+AUTH_LDAP_USER_DN_GROUP_OBJECT = os.getenv('AUTH_LDAP_USER_DN_GROUP_OBJECT')
+AUTH_LDAP_USER_DN_GROUP_NAME = os.getenv('AUTH_LDAP_USER_DN_GROUP_NAME')
+ldapgrouptype = os.getenv('AUTH_LDAP_GROUP_TYPE')
+groupobj = os.getenv('AUTH_LDAP_USER_FLAGS_BY_GROUP_OBJECT')
+groupname= os.getenv('AUTH_LDAP_USER_FLAGS_BY_GROUP_NAME')
+requiredgroup = os.getenv('AUTH_LDAP_REQUIRE_GROUP')
 
 AUTH_LDAP_SERVER_URI = f"ldaps://{hostname}"
 
@@ -27,28 +33,30 @@ if ACTIVE_DIRECTORY == 'true':
 		ldap.OPT_REFERRALS: 0
 	}
 
-AUTH_LDAP_BIND_DN = f"UID=netbox,CN=users,DC={dc1},DC={dc2}"
+AUTH_LDAP_BIND_DN = f"UID={binduser},{groupobj}={bindgroup},DC={dc1},DC={dc2}"
 AUTH_LDAP_BIND_PASSWORD = bindpassword
 
 LDAP_IGNORE_CERT_ERRORS = True
 
-from django_auth_ldap.config import LDAPSearch, PosixGroupType
+from django_auth_ldap.config import LDAPSearch, PosixGroupType, GroupOfNamesType
 
-AUTH_LDAP_USER_DN_TEMPLATE = f"uid=%(user)s,cn=users,dc={dc1},dc={dc2}"
+AUTH_LDAP_USER_DN_TEMPLATE = f"uid=%(user)s,{AUTH_LDAP_USER_DN_GROUP_OBJECT}={AUTH_LDAP_USER_DN_GROUP_NAME},dc={dc1},dc={dc2}"
 
 AUTH_LDAP_GROUP_SEARCH = LDAPSearch(f"dc={dc1},dc={dc2}", ldap.SCOPE_SUBTREE,
 																		f"(objectClass={AUTH_LDAP_GROUP_SEARCH_ROLE})")
-AUTH_LDAP_GROUP_TYPE = PosixGroupType()
+if ldapgrouptype == 'PosixGroupType':
+	AUTH_LDAP_GROUP_TYPE = PosixGroupType()
+else:
+	AUTH_LDAP_GROUP_TYPE = GroupOfNamesType()
 
-AUTH_LDAP_REQUIRE_GROUP = f"cn={bindgroup},cn=groups,dc={dc1},dc={dc2}"
+AUTH_LDAP_REQUIRE_GROUP = f"cn={requiredgroup},{groupobj}={groupname},dc={dc1},dc={dc2}"
 AUTH_LDAP_USER_FLAGS_BY_GROUP = {
-		"is_staff": f"cn={AUTH_LDAP_USER_STAFF},cn=groups,dc={dc1},dc={dc2}",
-    "is_superuser": f"cn={AUTH_LDAP_USER_SUPERUSER},cn=groups,dc={dc1},dc={dc2}"
+		"is_staff": f"cn={AUTH_LDAP_USER_STAFF},{groupobj}={groupname},dc={dc1},dc={dc2}",
+    "is_superuser": f"cn={AUTH_LDAP_USER_SUPERUSER},{groupobj}={groupname},dc={dc1},dc={dc2}"
 }
 
 if USER_FIRSTNAME_MAP is not None and USER_LASTNAME_MAP is not None:
 	AUTH_LDAP_USER_ATTR_MAP = { 
-	   "first_name": "cn",
-	   "last_name":  "sn"
+	   "first_name": f"{USER_FIRSTNAME_MAP}",
+	   "last_name":  f"{USER_LASTNAME_MAP}"
 	}
-
